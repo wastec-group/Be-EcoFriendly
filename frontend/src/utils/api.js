061f8a4c5e02ddabeba10809327bc315dbf5e2c8
wastec-group <wastec.tech@gmail.com>
2026-02-19 20:19:34 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log('Current API URL:', apiUrl);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api', // Use environment variable for production
+  // baseURL: import.meta.env.VITE_API_URL || '/api', // Use environment variable for production
+    baseURL: apiUrl || '/api', // Use environment variable for production
   headers: {
     'Content-Type': 'application/json',
   },
