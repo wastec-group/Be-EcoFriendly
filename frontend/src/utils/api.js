a70abfe76d9a0ba58d1abe0e2d3dcdd60d5c324f
wastec-group <wastec.tech@gmail.com>
2026-02-19 20:22:04 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || 'https://beecofriendly.onrender.com/api';
 console.log('Current API URL:', apiUrl);
 
 const api = axios.create({
